8f9f1b79707a4f4ec42c44b1a4f2fd5b058dcd8d
Shin Yamaga <shinyamaga@ShinnoMacBook-Air.local>
2023-11-02 09:13:12 +0900

    implemented object state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 const App = props => {
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
-  const reset = () => { 
-    setPrice(props.price)
-    setName(props.name)
-  }
+  const [state, setState] = useState(props);
+  const { itemName, price } = state;
   return (
     <>
-      <p>現在の{name}は、{price}円です</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name} onChange={ e => setName(e.target.value) } />
+      <p>現在の{itemName}は、{price}円です</p>
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input value={itemName} onChange={ e => setState({...state, itemName: e.target.value}) } />
     </>
   )
 }
 
 App.defaultProps = {
-  name: 'サンプル',
+  itemName: '',
   price: 0
 }
 
